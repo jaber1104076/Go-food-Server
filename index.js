106c90ffbfc6d719a72cc1a40f77c5b1a50467cc
jaber1104076 <jabercuet11@gmail.com>
2022-11-08 23:21:13 +0600

    id based services get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -23,6 +23,26 @@ async function run() {
             const query = req.body;
             const result = await serviceCollection.insertOne(query)
             res.send(result)
+        })
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const service = await cursor.limit(3).toArray()
+            res.send(service)
+        })
+        app.get('/all-services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const service = await cursor.toArray()
+            res.send(service)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const cursor = serviceCollection.find(query)
+            const service = await cursor.toArray()
+            res.send(service)
         })
 
 
